4730f937cc33e76a1b48b5376e13861994a26bfb
Zandakumsha <zandaj.kumsha@gmail.com>
2026-05-27 13:05:26 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,17 @@
+// Karah: I've created a function called greet that takes a name parameter and I've made sure that the code will be able to print a greeting message to the console when the function is called with a name argument.
+// I need help with creating a function called add that takes two parameters and makes the add function return the sum of both parameters.
+//Zanda do mind helping me?
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+// No problem KaraH I've got you.
+//Zanda here.
+//I will be helping Karah wth creating a function called add that takes two parameters and makes the add function return the sum of both parameters.
+function add(a, b) {
+  return a + b;
+}
+//Purrity here, i will call both functions and store their results in variables and print the results to the console
+let greeting = greet("Alice");
+let sum = add(5, 3);
+console.log(greeting);
+console.log(sum);
